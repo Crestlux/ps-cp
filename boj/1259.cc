1f4b9e2483dea739df988a245bc4f7b07789aae7
Crestlux <hemi0213@gmail.com>
2021-12-25 00:13:59 +0900

    Create 1259.cc
    백준 1259 - 팰린드롬수
    https://www.acmicpc.net/problem/1259

---
+++ boj/1259.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    while (1) {
+        int num; string s1, s2;
+        cin >> num;
+        if (num == 0) break;
+        s2 = s1 = to_string(num);
+        reverse(s2.begin(), s2.end());
+        if (s1 == s2) cout << "yes\n";
+        else cout << "no\n";
+    }
+    return 0;
+}
