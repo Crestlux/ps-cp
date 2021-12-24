43c8a64e54ee0cdbc8788dadf53fbaf5a527846d
Crestlux <hemi0213@gmail.com>
2021-12-25 00:18:22 +0900

    Create 10808.cc
    백준 10808 - 알파벳 개수
    https://www.acmicpc.net/problem/10808

---
+++ boj/10808.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int alph[26] = { 0, };
+    string s1;
+    cin >> s1;
+    int len = s1.length();
+    for (int i = 0; i < len; i++) {
+        alph[s1.at(i) - 'a']++;
+    }
+    for (const auto &ele : alph) {
+        cout << ele << " ";
+    }
+    return 0;
+}
