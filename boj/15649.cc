426891d6d664cc9c37dbcfaefbb923cc6688cd02
Crestlux <hemi0213@gmail.com>
2021-12-28 23:02:02 +0900

    Create 15649.cc
    백준 15649 - N과 M (1)
    https://www.acmicpc.net/problem/15649

---
+++ boj/15649.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pick(int n, vector<int> &picked, int toPick) {
+    if (toPick == 0) {
+        for (const auto &ele : picked) {
+            cout << ele << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int next = 1; next <= n; ++next) {
+        if (!picked.empty()) {
+            bool same = false;
+            for (const auto &ele : picked) {
+                if (next == ele) same = true;
+            }
+            if (same == true) continue;
+        }
+        picked.push_back(next);
+        pick(n, picked, toPick - 1);
+        picked.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, M; vector<int> v;
+    cin >> N >> M;
+    pick(N, v, M);
+    return 0;
+}
