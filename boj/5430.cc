22684d65b1f32f9224cd7d9c9e997ff34bd1f8d4
Crestlux <hemi0213@gmail.com>
2022-01-08 04:54:43 +0900

    AC
    백준 5430 - AC
    https://www.acmicpc.net/problem/5430

--- boj/5430.cc
+++ boj/5430.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
 	string p, temparr, tempnum; int T, n, templen;
 	deque<int> arr;
 	cin >> T;
