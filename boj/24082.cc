548878a86150a5213449ae70f404272f0377b617
Crestlux <hemi0213@gmail.com>
2022-01-11 03:27:34 +0900

    立方体 (Cube)
    백준 24082 - 立方体 (Cube)
    https://www.acmicpc.net/problem/24082

---
+++ boj/24082.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int num;
+    cin >> num;
+    cout << static_cast<int>(pow(num, 3)) << "\n";
+    return 0;
+}
