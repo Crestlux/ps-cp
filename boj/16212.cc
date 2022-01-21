e753839d4ffb30dc9fa0181b930f878664e30084
Crestlux <hemi0213@gmail.com>
2022-01-22 02:59:24 +0900

    정열적인 정렬
    백준 16212 - 정열적인 정렬
    https://www.acmicpc.net/problem/16212

---
+++ boj/16212.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int N; vector<int> v;
+    cin >> N;
+    v.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (const auto &ele : v) {
+        cout << ele << " ";
+    }
+    return 0;
+}
