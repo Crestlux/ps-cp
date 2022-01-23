1954a40e1049df5919e095c8ab318a61fa949b57
Crestlux <hemi0213@gmail.com>
2022-01-23 16:40:22 +0900

    웰컴
    백준 5337 - 웰컴
    https://www.acmicpc.net/problem/5337

---
+++ boj/5337.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s1 = ".  .   .", s2 = "|  | _ | _. _ ._ _  _", s3 = "|/\\|(/.|(_.(_)[ | )(/.";
+	cout << s1 << "\n" << s2 << "\n" << s3 << "\n";
+	return 0;
+}
